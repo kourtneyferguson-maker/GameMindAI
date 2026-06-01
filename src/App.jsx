31b6d7fd12c402479c5c6e9e3461256114511de9
VELLA <kourtneyferguson@hotmail.com>
2026-06-01 16:04:56 +1000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        background: "#030611",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
+      GamingMindAI
+    </div>
+  );
+}
